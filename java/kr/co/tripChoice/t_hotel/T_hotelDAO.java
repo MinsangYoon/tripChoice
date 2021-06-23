08e78c63b2793cb2d7443056b9b33d818daa86f3
MinsangYoon <85989489+MinsangYoon@users.noreply.github.com>
2021-06-23 15:53:49 +0900

    Update T_hotelDAO.java

--- java/kr/co/tripChoice/t_hotel/T_hotelDAO.java
+++ java/kr/co/tripChoice/t_hotel/T_hotelDAO.java
@@ -29,8 +29,8 @@ public class T_hotelDAO {
 	
 	//비즈니스로직 구현
 	public void create(T_hotelDTO dto) {
+		int cnt=0;
 		try {
-			int cnt=0;
 			con = dbopen.getConnection();
 			sql = new StringBuilder();
 			sql.append(" INSERT INTO t_hotel(th_code,th_name,th_room,th_reg,th_content,th_max) ");
@@ -47,10 +47,10 @@ public class T_hotelDAO {
 			System.out.println("숙소등록실패:"+e);
 		}finally {
 			DBClose.close(con, pstmt);
-		}
+		}//end
 	}//create() end
 	
-	public ArrayList<T_hotelDTO> list() {
+	public ArrayList<T_hotelDTO> list_admin() {
 		//DB에서 가져온 데이터(rs)를 한꺼번에 모아서(ArrayList)
 		list=null;
 		
@@ -85,7 +85,7 @@ public class T_hotelDAO {
 		}//end
 		
 		return list;
-	}//list() end
+	}//list_admin() end
 	
 	public T_hotelDTO read(String th_code) {
 		T_hotelDTO dto = null;
@@ -116,5 +116,67 @@ public class T_hotelDAO {
 	}//read() end
 	
 	
+	public int update(T_hotelDTO dto) {
+		int cnt=0;
+		try {
+			con = dbopen.getConnection();
+			sql = new StringBuilder();
+			sql.append(" UPDATE t_hotel ");
+			sql.append(" SET th_code=?,th_name=?,th_room=?,th_reg=?,th_content=?,th_max=? ");
+			sql.append(" WHERE th_code=? ");
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, dto.getTh_code());
+			pstmt.setString(2, dto.getTh_name());
+			pstmt.setString(3, dto.getTh_room());
+			pstmt.setString(4, dto.getTh_reg());
+			pstmt.setString(5, dto.getTh_content());
+			pstmt.setInt(6, dto.getTh_max());
+			pstmt.setString(7, dto.getTh_code());
+			cnt = pstmt.executeUpdate();
+		}catch(Exception e){
+			System.out.println("숙소수정실패:"+e);
+		}finally {
+			DBClose.close(con, pstmt);
+		}//end
+		
+		return cnt;
+	}//update() end
+	
+	public int delete(String th_code, String tu_id, String tu_pw) {
+		int cnt=0;
+		int x=0;
+		try {
+			//관리자 id와 비번이 맞다면 삭제진행
+			con = dbopen.getConnection();
+			sql = new StringBuilder();
+			sql.append(" SELECT count(*) ");
+			sql.append(" FROM tuser ");
+			sql.append(" WHERE tu_id=? AND tu_pw=? ");
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, tu_id);
+			pstmt.setString(2, tu_pw);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				x=rs.getInt(1);
+			}//if end
+			
+			if(x==1) { //x가 1이라는 건 관리자 id와 비번이 맞다는 뜻
+				sql.delete(0, sql.length());
+				sql.append(" DELETE FROM t_hotel ");
+				sql.append(" WHERE th_code=? ");
+				pstmt = con.prepareStatement(sql.toString());
+				pstmt.setString(1, th_code);
+				cnt=pstmt.executeUpdate();
+			}//if end
+			
+		}catch(Exception e){
+			System.out.println("숙소수정실패:"+e);
+		}finally {
+			DBClose.close(con, pstmt);
+		}//end
+		
+		return cnt;
+	}//delete() end
 	
 }//class end
