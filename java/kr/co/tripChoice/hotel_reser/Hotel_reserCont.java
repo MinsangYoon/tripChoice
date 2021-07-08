22ebd30f11072a665e31bf820247fd526d70745b
MinsangYoon <85989489+MinsangYoon@users.noreply.github.com>
2021-07-08 10:49:46 +0900

    Update Hotel_reserCont.java

--- java/kr/co/tripChoice/hotel_reser/Hotel_reserCont.java
+++ java/kr/co/tripChoice/hotel_reser/Hotel_reserCont.java
@@ -19,38 +19,25 @@ public class Hotel_reserCont {
 	@Autowired
 	private Hotel_reserDAO dao;
 	
+	
 	public Hotel_reserCont() {//기본생성자함수
 		System.out.println("----Hotel_reserCont() 객체 생성됨");
 	}//end
 	
 	
 	@RequestMapping("hotel_reser/hotel_reser_insert.do")
-	public String hotel_reser_insert(HttpServletRequest req, Model model) {
-		String trip_code = req.getParameter("trip_code");
-		int tr_adult = Integer.parseInt(req.getParameter("tr_adult"));
-		int tr_kid = Integer.parseInt(req.getParameter("tr_kid"));
-		int tr_baby = Integer.parseInt(req.getParameter("tr_baby"));
-		int tr_price = Integer.parseInt(req.getParameter("tr_price"));
-		String tr_departure = req.getParameter("tr_departure");
-		String tr_arrival = req.getParameter("tr_arrival");
-		String th_code = req.getParameter("th_code");
-		String tu_id = req.getParameter("tu_id");
-		int thr_member = Integer.parseInt(req.getParameter("thr_member"));
+	public String hotel_reser_insert(Hotel_reserDTO dto, Model model) {
+		int cnt = dao.create(dto);
 		
-		dao.create2(th_code,tu_id,thr_member,trip_code);
-		
-		model.addAttribute("tr_adult",tr_adult);
-		model.addAttribute("tr_kid",tr_kid);
-		model.addAttribute("tr_baby",tr_baby);
-		model.addAttribute("tr_price",tr_price);
-		model.addAttribute("tr_departure",tr_departure);
-		model.addAttribute("tr_arrival",tr_arrival);
-		model.addAttribute("trip_code",trip_code);
-		model.addAttribute("tu_id",tu_id);
-		
-		return "redirect:/trip_reser/trip_reser_insert.do";
-		
+		if(cnt==1) {
+		return "hotel_reser/msg";
+		}else {
+			return "";
+		}
 	}//end
+	
+	
+	
 	
 
 }//class end
