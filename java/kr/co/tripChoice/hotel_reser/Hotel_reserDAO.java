36db0e63eba1f105ca4ca9dfcbde9ed4db445885
MinsangYoon <85989489+MinsangYoon@users.noreply.github.com>
2021-07-08 10:50:12 +0900

    Update Hotel_reserDAO.java

--- java/kr/co/tripChoice/hotel_reser/Hotel_reserDAO.java
+++ java/kr/co/tripChoice/hotel_reser/Hotel_reserDAO.java
@@ -33,15 +33,21 @@ public class Hotel_reserDAO {
 		try {
 			con = dbopen.getConnection();
 			sql = new StringBuilder();
-			sql.append(" INSERT INTO hotel_reser(th_code,tu_id,thr_member,trip_code) ");
-			sql.append(" VALUES( ?,?,?,? ) ");
+			sql.append(" INSERT INTO hotel_reser(th_code,tu_id,thr_adult,thr_kid,thr_baby,thr_price,thr_resdate,thr_in,thr_out) ");
+			sql.append(" VALUES( ?,?,?,?,?,?,now(),?,? ) ");
 			pstmt = con.prepareStatement(sql.toString());
 			pstmt.setString(1, dto.getTh_code());
 			pstmt.setString(2, dto.getTu_id());
-			pstmt.setInt(3, dto.getThr_member());
-			pstmt.setString(4, dto.getTrip_code());
+			pstmt.setInt(3, dto.getThr_adult());
+			pstmt.setInt(4, dto.getThr_kid());
+			pstmt.setInt(5, dto.getThr_baby());
+			pstmt.setInt(6, dto.getThr_price());
+			pstmt.setString(7, dto.getThr_in());
+			pstmt.setString(8, dto.getThr_out());
+			
 			cnt = pstmt.executeUpdate();
 			
+			/*
 			//예약가능여부 'N'으로 변경하기
 			sql.delete(0, sql.length()); //위에서 썼던 sql문 초기화.
 			sql.append(" UPDATE t_hotel ");
@@ -50,6 +56,7 @@ public class Hotel_reserDAO {
 			pstmt = con.prepareStatement(sql.toString());
 			pstmt.setString(1, dto.getTh_code());
 			pstmt.executeUpdate();
+			*/
 			
 		}catch(Exception e){
 			System.out.println("숙소예약실패:"+e);
@@ -60,38 +67,7 @@ public class Hotel_reserDAO {
 		return cnt;
 	}//create() end
 	
-	public int create2(String th_code, String tu_id, int thr_member, String trip_code) { //숙소예약테이블에 insert
-		int cnt=0;
-		try {
-			con = dbopen.getConnection();
-			sql = new StringBuilder();
-			sql.append(" INSERT INTO hotel_reser(th_code,tu_id,thr_member,trip_code) ");
-			sql.append(" VALUES( ?,?,?,? ) ");
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, th_code);
-			pstmt.setString(2, tu_id);
-			pstmt.setInt(3, thr_member);
-			pstmt.setString(4, trip_code);
-			cnt = pstmt.executeUpdate();
-			
-			//예약가능여부 'N'으로 변경하기
-			sql.delete(0, sql.length()); //위에서 썼던 sql문 초기화.
-			sql.append(" UPDATE t_hotel ");
-			sql.append(" SET th_situation='N' ");
-			sql.append(" WHERE th_code=? ");
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, th_code);
-			pstmt.executeUpdate();
-			
-		}catch(Exception e){
-			System.out.println("숙소예약실패:"+e);
-		}finally {
-			DBClose.close(con, pstmt);
-		}//end
-		
-		return cnt;
-	}//create() end
-	
+
 	
 	
 	
